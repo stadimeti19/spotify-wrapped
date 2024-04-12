03203c77320e771e3dc933011aa33e478fbda2d2
HenryLinLin <70624851+HenryLinLin@users.noreply.github.com>
2024-04-12 15:03:21 -0400

    username

--- app/src/main/java/com/example/spotify_wrapped/IntroActivity.java
+++ app/src/main/java/com/example/spotify_wrapped/IntroActivity.java
@@ -9,23 +9,30 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class IntroActivity extends AppCompatActivity {
     private TextView welcomeText;
     private TextView tapToContinueText;
     private ImageView imageViewSetting;
-
     private ImageView imageViewHome;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_page);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String username = currentUser != null ? currentUser.getDisplayName() : "User";
+
         int score = getIntent().getIntExtra("score", 0);
 
         welcomeText = findViewById(R.id.welcomeText);
         tapToContinueText = findViewById(R.id.tapToContinueText);
-        welcomeText.setText("Welcome, Henry, you got " + score + "!");
-
+        welcomeText.setText("Welcome, " + username + ", you got " + score + "!");
 
         // Set up touch listener on the layout to detect screen tap
         View introLayout = findViewById(R.id.introLayout);
@@ -37,6 +44,7 @@ public class IntroActivity extends AppCompatActivity {
                 return true;
             }
         });
+
         imageViewSetting = findViewById(R.id.settings_button);
         imageViewSetting.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +52,7 @@ public class IntroActivity extends AppCompatActivity {
                 startActivity(new Intent(IntroActivity.this, SettingsPage.class));
             }
         });
+
         imageViewHome = findViewById(R.id.home_button);
         imageViewHome.setOnClickListener(new View.OnClickListener() {
             @Override
