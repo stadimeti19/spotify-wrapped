2a9acad0a56fa31d1c75f0b2fbc7af61496640f4
HenryLinLin <70624851+HenryLinLin@users.noreply.github.com>
2024-04-12 18:57:27 -0400

    randomized choices

--- app/src/main/java/com/example/spotify_wrapped/GamePage.java
+++ app/src/main/java/com/example/spotify_wrapped/GamePage.java
@@ -14,13 +14,23 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class GamePage extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
     private int score = 0;
+    private Random random = new Random();
+
+    private String correctSong;
+    private String correctArtist;
+    private String correctGenre;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,22 +51,8 @@ public class GamePage extends AppCompatActivity {
                             if (document != null && document.exists()) {
                                 List<String> songs = (List<String>) document.get("songs");
                                 if (songs != null && !songs.isEmpty()) {
-                                    populateTopSongs(songs);
-                                }
-                            }
-                        } else {
-                        }
-                    });
-
-            // Populate top genres
-            db.collection("users").document(userId).get()
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document != null && document.exists()) {
-                                List<String> genres = (List<String>) document.get("genres");
-                                if (genres != null && !genres.isEmpty()) {
-                                    populateTopGenres(genres);
+                                    List<String> formattedSongs = removePrefix(songs);
+                                    populateTopSongs(formattedSongs);
                                 }
                             }
                         } else {
@@ -64,7 +60,22 @@ public class GamePage extends AppCompatActivity {
                         }
                     });
 
-            // Populate top artists
+            db.collection("users").document(userId).get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document != null && document.exists()) {
+                                List<String> genres = (List<String>) document.get("genres");
+                                if (genres != null && !genres.isEmpty()) {
+                                    List<String> formattedGenres = removePrefix(genres);
+                                    populateTopGenres(formattedGenres);
+                                }
+                            }
+                        } else {
+                            // Handle errors
+                        }
+                    });
+
             db.collection("users").document(userId).get()
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
@@ -72,7 +83,8 @@ public class GamePage extends AppCompatActivity {
                             if (document != null && document.exists()) {
                                 List<String> artists = (List<String>) document.get("artists");
                                 if (artists != null && !artists.isEmpty()) {
-                                    populateTopArtists(artists);
+                                    List<String> formattedArtists = removePrefix(artists);
+                                    populateTopArtists(formattedArtists);
                                 }
                             }
                         } else {
@@ -82,19 +94,25 @@ public class GamePage extends AppCompatActivity {
 
             Button submitButton = findViewById(R.id.submitButton);
             submitButton.setOnClickListener(view -> {
-                checkAnswer(R.id.answer1RadioButton, "topSong");
+                RadioGroup radioGroup = findViewById(R.id.answersRadioGroup);
+                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                checkAnswer(checkedRadioButtonId, correctSong);
                 submitButton.setEnabled(false);
             });
 
             Button submitButton2 = findViewById(R.id.submitButton2);
             submitButton2.setOnClickListener(view -> {
-                checkAnswer(R.id.answer1RadioButton2, "topArtist");
+                RadioGroup radioGroup = findViewById(R.id.answersRadioGroup2);
+                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                checkAnswer(checkedRadioButtonId, correctGenre);
                 submitButton2.setEnabled(false);
             });
 
             Button submitButton3 = findViewById(R.id.submitButton3);
             submitButton3.setOnClickListener(view -> {
-                checkAnswer(R.id.answer1RadioButton3, "topGenre");
+                RadioGroup radioGroup = findViewById(R.id.answersRadioGroup3);
+                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                checkAnswer(checkedRadioButtonId, correctArtist);
                 Intent intent = new Intent(GamePage.this, IntroActivity.class);
                 intent.putExtra("score", score); // Pass the score as an extra
                 startActivity(intent);
@@ -103,86 +121,95 @@ public class GamePage extends AppCompatActivity {
     }
 
     private void populateTopSongs(List<String> songs) {
-        RadioButton radioButton1 = findViewById(R.id.answer1RadioButton);
-        RadioButton radioButton2 = findViewById(R.id.answer2RadioButton);
-        RadioButton radioButton3 = findViewById(R.id.answer3RadioButton);
-        RadioButton radioButton4 = findViewById(R.id.answer4RadioButton);
+        RadioButton[] radioButtons = {
+                findViewById(R.id.answer1RadioButton),
+                findViewById(R.id.answer2RadioButton),
+                findViewById(R.id.answer3RadioButton),
+                findViewById(R.id.answer4RadioButton)
+        };
 
-        if (songs.size() >= 1) {
-            radioButton1.setText(songs.get(0));
+        correctSong = songs.get(0);
+        List<String> otherSongs = new ArrayList<>(songs.subList(1, songs.size()));
+        Collections.shuffle(otherSongs);
+        int correctIndex = random.nextInt(4);
+        radioButtons[correctIndex].setText(songs.get(0));
+        for (int i = 0, j = 0; i < 4; i++) {
+            if (i != correctIndex) {
+                radioButtons[i].setText(otherSongs.get(j++));
+            }
         }
-        if (songs.size() >= 2) {
-            radioButton2.setText(songs.get(1));
-        }
-        if (songs.size() >= 3) {
-            radioButton3.setText(songs.get(2));
-        }
-        if (songs.size() >= 4) {
-            radioButton4.setText(songs.get(3));
-        }
+        shuffleRadioButtons(radioButtons);
     }
 
     private void populateTopGenres(List<String> genres) {
-        RadioButton radioButton1 = findViewById(R.id.answer1RadioButton2);
-        RadioButton radioButton2 = findViewById(R.id.answer2RadioButton2);
-        RadioButton radioButton3 = findViewById(R.id.answer3RadioButton2);
-        RadioButton radioButton4 = findViewById(R.id.answer4RadioButton2);
+        RadioButton[] radioButtons = {
+                findViewById(R.id.answer1RadioButton2),
+                findViewById(R.id.answer2RadioButton2),
+                findViewById(R.id.answer3RadioButton2),
+                findViewById(R.id.answer4RadioButton2)
+        };
 
-        if (genres.size() >= 1) {
-            radioButton1.setText(genres.get(0));
+        correctGenre = genres.get(0);
+        List<String> otherGenres = new ArrayList<>(genres.subList(1, genres.size()));
+        Collections.shuffle(otherGenres);
+        int correctIndex = random.nextInt(4);
+        radioButtons[correctIndex].setText(genres.get(0));
+        for (int i = 0, j = 0; i < 4; i++) {
+            if (i != correctIndex) {
+                radioButtons[i].setText(otherGenres.get(j++));
+            }
         }
-        if (genres.size() >= 2) {
-            radioButton2.setText(genres.get(1));
-        }
-        if (genres.size() >= 3) {
-            radioButton3.setText(genres.get(2));
-        }
-        if (genres.size() >= 4) {
-            radioButton4.setText(genres.get(3));
-        }
+        shuffleRadioButtons(radioButtons);
     }
 
     private void populateTopArtists(List<String> artists) {
-        RadioButton radioButton1 = findViewById(R.id.answer1RadioButton3);
-        RadioButton radioButton2 = findViewById(R.id.answer2RadioButton3);
-        RadioButton radioButton3 = findViewById(R.id.answer3RadioButton3);
-        RadioButton radioButton4 = findViewById(R.id.answer4RadioButton3);
+        RadioButton[] radioButtons = {
+                findViewById(R.id.answer1RadioButton3),
+                findViewById(R.id.answer2RadioButton3),
+                findViewById(R.id.answer3RadioButton3),
+                findViewById(R.id.answer4RadioButton3)
+        };
 
-        if (artists.size() >= 1) {
-            radioButton1.setText(artists.get(0));
-        }
-        if (artists.size() >= 2) {
-            radioButton2.setText(artists.get(1));
-        }
-        if (artists.size() >= 3) {
-            radioButton3.setText(artists.get(2));
-        }
-        if (artists.size() >= 4) {
-            radioButton4.setText(artists.get(3));
-        }
-    }
-
-    private void populateRadioGroup(RadioGroup radioGroup, List<String> items) {
-        for (int i = 0; i < Math.min(items.size(), 4); i++) {
-            RadioButton radioButton = new RadioButton(this);
-            radioButton.setText(items.get(i));
-            radioGroup.addView(radioButton);
-        }
-    }
-
-    private void checkAnswer(int radioButtonId, String category) {
-        RadioButton radioButton = findViewById(radioButtonId);
-        if (radioButton != null && radioButton.isChecked()) {
-            if (category.equals("topSong") && radioButtonId == R.id.answer1RadioButton) {
-                score++;
-            } else if (category.equals("topArtist") && radioButtonId == R.id.answer1RadioButton2) {
-                score++;
-            } else if (category.equals("topGenre") && radioButtonId == R.id.answer1RadioButton3) {
-                score++;
+        correctArtist = artists.get(0);
+        List<String> otherArtists = new ArrayList<>(artists.subList(1, artists.size()));
+        Collections.shuffle(otherArtists);
+        int correctIndex = random.nextInt(4);
+        radioButtons[correctIndex].setText(artists.get(0));
+        for (int i = 0, j = 0; i < 4; i++) {
+            if (i != correctIndex) {
+                radioButtons[i].setText(otherArtists.get(j++));
             }
-            Toast.makeText(this, "Score: " + score, Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Wrong answer dumbass", Toast.LENGTH_SHORT).show();
         }
+        shuffleRadioButtons(radioButtons);
+    }
+
+    private void shuffleRadioButtons(RadioButton[] radioButtons) {
+        List<RadioButton> radioButtonList = Arrays.asList(radioButtons);
+        Collections.shuffle(radioButtonList);
+        for (int i = 0; i < 4; i++) {
+            radioButtons[i] = radioButtonList.get(i);
+        }
+    }
+
+    private void checkAnswer(int checkedRadioButtonId, String correctValue) {
+        RadioButton selectedRadioButton = findViewById(checkedRadioButtonId);
+        if (selectedRadioButton != null) {
+            String selectedText = selectedRadioButton.getText().toString();
+            if (selectedText.equals(correctValue)) {
+                score++;
+                Toast.makeText(this, "Correct! Score: " + score, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Wrong answer!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Please select an option", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private List<String> removePrefix(List<String> strings) {
+        List<String> formattedStrings = new ArrayList<>();
+        for (String str : strings) {
+            formattedStrings.add(str.replaceAll("^\\d+\\.\\s*", ""));
+        }
+        return formattedStrings;
     }
 }
